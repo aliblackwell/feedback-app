94a82fb56bbf2000a34af15628fe703a6f6f10bd
Alasdair Blackwell <Alasdair@ada.ac.uk>
2019-01-18 14:58:31 +0000

    Working pouchdb and messages

--- app.js
+++ app.js
@@ -1,33 +1,44 @@
-var db = new PouchDB('my_database');
-console.log('Hello world')
-console.log(db)
-
-var doc = {
-  "_id": "messages",
-  "messages": [
-    "I'm a test message hard-coded in the app",
-    "I'm another hard-coded message and I'm a bit longer than the first message to make sure we create a stylesheet that works for long messages as well as short ones.",
-    "I'm another",
-    "And another"
-  ]
-};
+var db = new PouchDB('app_database')
 
 
 db.get('messages').then(function (doc) {
-  console.log(doc);
-});
+  renderMessages(doc);
+}).catch(function (e) {
+  if (e.message == 'missing') {
+    db.put({
+      "_id": "messages",
+      "messages": [
+        "I'm a test message hard-coded in the app"
+      ]
+    }).then(function (response) {
+      db.get('messages').then(function (doc) {
+        renderMessages(doc)
+      })
+    })
+  }
+})
+
+function renderMessages(doc) {
+  const formattedMessages = doc.messages.map(msg => `<p class="message-item">${msg}</p>`).join('')
+  document.querySelector('#messages-container').innerHTML = formattedMessages
+}
+
 
 const form = document.querySelector('form')
 form.addEventListener('submit', event => {
   event.preventDefault()
-  console.log(event)
+
   const newMessage = document.querySelector('textarea').value
-  console.log(newMessage)
+
   db.get('messages').then(doc => {
     doc.messages.push(newMessage)
+    renderMessages(doc)
+    document.querySelector('textarea').value = ''
     return db.put(doc)
   }).catch(err => {
     console.log(err)
   })
 })
+
+// db.replicate.to('https://feedback-app-db-server.aliblackwell.me')
 
